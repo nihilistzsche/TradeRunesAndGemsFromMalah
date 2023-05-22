95b3ed5dc1c4e5c6b0747fee0faf02331efacb0e
Nihilistzsche <nihilistzsche@gmail.com>
2023-05-22 00:03:04 -0700

    First version.

---
+++ mod.js
@@ -0,0 +1,19 @@
+const miscFilename = 'global\\excel\\misc.txt';
+const misc = D2RMM.readTsv(miscFilename);
+
+misc.rows.forEach((row) => {
+    if (row.type.includes("gem") && (row.name.includes("Chipped") || row.name.includes("Flawed"))) {
+        row.MalahMin = 1
+        row.MalahMax = 1
+        row.PermStoreItem = 1
+    }
+    if (row.type === "rune" && (['Jah Rune', 'Cham Rune', 'Zod Rune'].indexOf(row.name) === -1)) {
+        if (['El Rune', 'Eld Rune', 'Tir Rune', 'Nef Rune', 'Eth Rune', 'Ith Rune', 'Tal Rune', 'Ral Rune', 'Ort Rune', 'Thul Rune'].indexOf(row.name) !== -1) {
+            row.MalahMin = 1
+            row.MalahMax = 1
+        }
+        row.PermStoerItem = 1
+    }
+})
+
+D2RMM.writeTsv(miscFilename, misc)
