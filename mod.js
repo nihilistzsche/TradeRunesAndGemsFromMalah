4180ed553f3a8d35d4a0b4466fa58864a1c20902
Nihilistzsche <nihilistzsche@gmail.com>
2023-05-29 18:02:10 -0700

    Default gems replace both chipped and flawed gems in Nightmare, Flawless replace chipped and Perfect flawed in Hell.  Fixed runes so that 1-11 are available in Normal, 12-22 are available in Nightmare, and 23-33 are available in Hell.

--- mod.js
+++ mod.js
@@ -1,16 +1,44 @@
 const miscFilename = 'global\\excel\\misc.txt';
 const misc = D2RMM.readTsv(miscFilename);
 
+const gem_upgrades = {
+    "Chipped ": [ "", "Flawless " ],
+    "Flawed ": [ "" , "Perfect " ]
+}
+
+const rune_upgrades = {
+    "El": [ "Sol", "Mal" ],
+    "Eld": [ "Shael", "Ist" ],
+    "Tir": [ "Dol", "Gul" ],
+    "Nef": [ "Hel", "Vex" ],
+    "Eth": [ "Io", "Ohm" ],
+    "Ith": [ "Lum", "Lo" ],
+    "Tal": [ "Ko", "Sur" ],
+    "Ral": [ "Fal", "Ber" ],
+    "Ort": [ "Lem", "Jah" ],
+    "Thul": [ "Pul", "Cham" ],
+    "Amn": [ "Um", "Zod" ]
+}
+
 misc.rows.forEach((row) => {
-    if (row.type.includes("gem") && (row.name.includes("Chipped") || row.name.includes("Flawed"))) {
-        row.MalahMin = 1
-        row.MalahMax = 1
+    if (row.type.includes("gem")) {
+        if (row.name.includes("Chipped") || row.name.includes("Flawed")) {
+            row.MalahMin = 1
+            row.MalahMax = 1
+            const upgrade_key = row.name.includes("Chipped") && "Chipped " || "Flawed ";
+            row.NightmareUpgrade = row.name.replace(upgrade_key, gem_upgrades[upgrade_key][0])
+            row.HellUpgrade = row.name.replace(upgrade_key, gem_upgrades[upgrade_key][1])
+        }
         row.PermStoreItem = 1
     }
     if (row.type === "rune") {
-        if (['El Rune', 'Eld Rune', 'Tir Rune', 'Nef Rune', 'Eth Rune', 'Ith Rune', 'Tal Rune', 'Ral Rune', 'Ort Rune', 'Thul Rune'].indexOf(row.name) !== -1) {
+        const rune_base = row.name.replace(" Rune", "")
+        if (rune_base in rune_upgrades) {
             row.MalahMin = 1
             row.MalahMax = 1
+            const rune_upgrade = rune_upgrades[rune_base]
+            row.NightmareUpgrade = rune_upgrade[0] + " Rune"
+            row.HellUpgrade = rune_upgrade[1] + " Rune"
         }
         row.PermStoreItem = 1
     }
